09993e64e6ca514e3d9ac4519f1572ee02b23dae
Dominic Hock <d.hock@it-hock.de>
2023-11-10 17:29:31 +0100

    chore: add config files

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('jest').Config} */
+const config = {
+    transform: {'^.+\\.ts?$': 'ts-jest'},
+    testEnvironment: 'node',
+    testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    silent: false,
+};
+
+module.exports = config;
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,59 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: './src/main.ts',
+    entry: ["./src/main.ts", "./src/sass/render.scss"],
     mode: "development",
+    //watch: true,
+    //devtool: "hidden-cheap-module-source-map",
+    devtool: "inline-source-map",
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
-                use: 'ts-loader',
+                use: "ts-loader",
                 exclude: /node_modules/,
+            },
+            /*{
+                test: /\.(eot|woff|woff2|ttf)(\?\S*)?$/,
+                loader: "file-loader",
+                options: {name: "[name].[ext]", outputPath: "fonts/",}
+            },*/
+            {
+                test: /\.(svg|png|jpe?g|gif)$/i,
+                loader: "file-loader",
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                exclude: /node_modules/,
+                include: [path.resolve(__dirname, "src/sass")],
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader"
+                    },
+                    {
+                        loader: "sass-loader"
+                    },
+                ],
             },
         ],
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "css/[name].css",
+            chunkFilename: "css/[id].css",
+        }),
+        /*new HtmlWebpackPlugin({
+            template: "src/html/index.html"
+        })*/
+    ],
     resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
+        extensions: [".tsx", ".ts", ".js", ".scss"],
     },
     output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+        filename: "js/bundle.js",
+        path: path.resolve(__dirname, "dist"),
     },
 };
